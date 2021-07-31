a44540cf4cb54c781199243dfa1d560c5e9084f9
krishpayyazhi <88235482+krishpayyazhi@users.noreply.github.com>
2021-07-31 17:06:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,400);
+  ball=createSprite(150,150,50,50);
+  ball.shapeColor="white"
+}
+
+function draw() 
+{
+  background(140);
+ drawSprites();
+}
+
+
+
+
+
